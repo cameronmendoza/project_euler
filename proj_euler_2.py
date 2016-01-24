2180cf34b1846aef622ba0fdac6f82b71ad85d17
Cameron Mendoza <msg.cam@gmail.com>
2016-01-24 21:34:40 +1100

    Initial commit

---
+++ proj_euler_2.py
@@ -0,0 +1,21 @@
+def fib(num):
+	if (num == 0):
+		return 0
+	elif(num == 1):
+		return 1
+	return fib(num - 1) + fib(num - 2)
+
+sum = 0
+index = 1
+while (fib(index) < 4000000):
+	# reduce testing for even values
+	# easy to prove that every third fib number is even
+	if index % 3 != 0:
+		index += 1
+		continue
+	if (fib(index) % 2 == 0):
+		sum += fib(index)
+	index += 1
+
+print sum
+
---
+++ proj_euler_3.py
@@ -0,0 +1,36 @@
+import sys
+
+# remove raw_input weirdness
+sys.stdout.flush()
+
+def isPrime(num):
+	# make sure num is a positive integer
+	num = abs(int(num))
+
+	if num < 2:
+		return False
+
+	# 2 is only even prime number
+	if num == 2:
+		return True
+	# all other even numbers not prime
+	if not num & 1:
+		return False
+
+	for x in range(3, int(num**0.5) + 1, 2):
+		if num % x == 0:
+			return False
+	return True
+
+def get_pfs(num):
+	list_of_pfs = []
+	for x in range(2, int(num**0.5) + 1):
+		if isPrime(x) and (num % x == 0):
+			list_of_pfs.append(x)
+	return list_of_pfs
+
+number = int(raw_input('Enter number to determine its largest prime factor: '))
+
+largest_prime_factor = max(get_pfs(number))
+
+print 'Largest prime factor of %d is %d' % (number, largest_prime_factor)
